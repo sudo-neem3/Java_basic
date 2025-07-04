ba35c32338498577baf41c70f59b0dc76cec4e10
Abeeha-khalid <neemkhalid0@gmail.com>
2025-07-04 12:07:36 -0700

    Array file created

---
+++ src/sw.java
@@ -0,0 +1,23 @@
+import java.sql.SQLOutput;
+
+public class sw {
+
+    public static void main(String[] args) {
+String
+input = "";
+        if(input =="")
+        {
+
+        }
+        else if( input == "")
+        {
+            System.out.println();
+            System.out.println();
+            System.out.println();
+        }
+
+
+    }
+
+
+}
